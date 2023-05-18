6391b0cb1c38d09f0759b9d919bad9ba314a0448
Mmeckawy <dmeckawy@gmail.com>
2023-05-19 02:33:26 +0300

    Added front-end

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.get('/', (req, res) => {
       // Process the ontology data as needed
       console.log(ontologyData);
 
-      // SPARQL query
-      const query = `
+      const defaultQuery = `
       PREFIX owl: <http://www.w3.org/2002/07/owl#>
       PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -23,10 +22,12 @@ app.get('/', (req, res) => {
 
       SELECT ?band
       WHERE {
-    ?band rdf:type ont:Band .
-    }
+        ?band rdf:type ont:Band .
+      }
+      `;
 
-    `;
+      // SPARQL query
+      const query = req.query.query || defaultQuery;
 
       const endpoint = 'http://localhost:3030/Ontology/sparql'; // Replace with your SPARQL endpoint URL
 
@@ -37,10 +38,61 @@ app.get('/', (req, res) => {
           res.status(500).send('Internal Server Error');
         } else {
           const bands = results.results.bindings.map(binding => binding.band.value);
-          console.log(bands.ontologyData);
-          let table = "<table><tr><td>"
-          table += "test hello</td></tr></table>"
-          res.send(table);
+          console.log(bands);
+
+          const tableHtml = generateTableHtml(bands);
+
+          const html = `
+          <html>
+            <head>
+              <style>
+                table {
+                  border-collapse: collapse;
+                  margin: 0 auto;
+                  text-align: center;
+                  width: 100%;
+                }
+                th, td {
+                  border: 1px solid black;
+                  padding: 8px;
+                }
+                th {
+                  background-color: #f2f2f2;
+                }
+                .grey-row {
+                  background-color: grey;
+                  color: white;
+                }
+                h1 {
+                  text-align: center;
+                }
+                input{
+                  width: 25%;
+                }
+                textarea{
+                  width: 100%;
+                }
+                .center-button {
+                  text-align: center;
+                }
+              </style>
+            </head>
+            <body>
+              <h1>SPARQL Query Results</h1>
+              <form action="/" method="get">
+                <label for="query">SPARQL Query:</label><br>
+                <textarea id="query" name="query" rows="14" cols="120" width="100%">${query}</textarea><br>
+                <div class="center-button">
+                  <input type="submit" value="Submit">
+                </div>
+              </form>
+              <br>
+              ${tableHtml}
+            </body>
+          </html>
+          `;
+
+          res.send(html);
         }
       });
     })
@@ -49,6 +101,30 @@ app.get('/', (req, res) => {
       res.status(500).send('Internal Server Error');
     });
 });
+
+function generateTableHtml(data) {
+  let tableHtml = '<table>';
+
+  // Generate table header
+  tableHtml += '<thead>';
+  tableHtml += '<tr>';
+  tableHtml += '<th>Band</th>';
+  tableHtml += '</tr>';
+  tableHtml += '</thead>';
+
+  // Generate table body
+  tableHtml += '<tbody>';
+  for (const item of data) {
+    tableHtml += '<tr>';
+    tableHtml += `<td>${item}</td>`;
+    tableHtml += '</tr>';
+  }
+  tableHtml += '</tbody>';
+
+  tableHtml += '</table>';
+
+  return tableHtml;
+}
 
 app.listen(8080, () => {
   console.log('Server started on port 8080');
